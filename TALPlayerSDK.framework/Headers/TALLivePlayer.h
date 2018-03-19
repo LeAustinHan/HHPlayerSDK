b6c49cf355704a0691a544c2a162ab881471ca8d
hanjize <hanjize@100tal.com>
2018-03-19 15:29:54 +0800

    add framework files

---
+++ TALPlayerSDK.framework/Headers/TALLivePlayer.h
@@ -0,0 +1,13 @@
+//
+//  TALPlayerWrap.h
+//  TALPlayerBirdge
+//
+//  Created by Sanji on 16/8/4.
+//  Copyright © 2016年 Sanji. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface TALLivePlayer : NSObject
+@end
